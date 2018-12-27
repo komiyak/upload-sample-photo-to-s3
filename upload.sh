875cdfb4deae3c2a3579552477d3d9eeaf46ca45
Kosuke Komiya <kosuke.komiya@gmail.com>
2018-12-27 12:54:49 +0900

    Add a rule of includeing png

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aws s3 cp photos/ s3://$1 --acl public-read --recursive --exclude "*" --include "*.jpg"
+aws s3 cp photos/ s3://$1 --acl public-read --recursive --exclude "*" --include "*.jpg" --include "*.png"
 
